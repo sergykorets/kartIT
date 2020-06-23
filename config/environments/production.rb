cc6a8be976a38625224e2ede019273347c89b10d
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2020-06-23 14:16:39 +0300

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,7 @@ Rails.application.configure do
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
-    :authentication       => "plain",
-    :enable_starttls_auto => true
+    :authentication       => "plain"
   }
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
