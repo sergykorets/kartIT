64b6632e412b6e76d60e382dbc8eb398c4aa72ce
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2021-03-23 15:48:33 +0200

    2021

--- app/models/race.rb
+++ app/models/race.rb
@@ -4,7 +4,7 @@ class Race < ApplicationRecord
   has_many :users, through: :race_standings
   belongs_to :best_lap_user, class_name: 'User', foreign_key: :best_lap_user_id, optional: true
 
-  enum season: ['2019', '2020']
+  enum season: ['2019', '2020', '2021']
   enum track: [:one, :one_r, :two, :two_r, :tree, :tree_r, :four, :four_r, :five, :five_r, :six, :six_r,
                        :seven, :seven_r, :eight, :eight_r, :nine, :nine_r, :ten, :ten_r, :eleven, :eleven_r,
                        :twelve, :twelve_r, :thirteen, :thirteen_r, :fourteen, :fourteen_r, :fifteen, :fifteen_r]
