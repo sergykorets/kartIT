b89961ade773e36560a5c4529bc18137104ccfb0
Sergii Fedorchuk <ext-sergii.fedorchuck@foodie.fm>
2021-07-19 19:50:41 +0300

    mailer fix

--- app/jobs/next_race_notifier_job.rb
+++ app/jobs/next_race_notifier_job.rb
@@ -2,6 +2,8 @@ class NextRaceNotifierJob < ApplicationJob
   queue_as :default
 
   def perform
-    ItRacingMailer.next_race_notifier.deliver
+    User.where("email NOT ILIKE ?", '%user.com').each do |user|
+      ItRacingMailer.next_race_notifier(user).deliver
+    end
   end
 end
--- app/mailers/it_racing_mailer.rb
+++ app/mailers/it_racing_mailer.rb
@@ -1,10 +1,9 @@
 class ItRacingMailer < ApplicationMailer
   default from: 'karting.it.racing@gmail.com'
 
-  def next_race_notifier
+  def next_race_notifier(user)
     @next_race = Race.last.date.strftime("%d.%m.%Y %H:%M")
-    users = User.where("email NOT ILIKE ?", 'racer%user.com').collect(&:email).join(", ")
-    mail(to: users, subject: "Наступна гонка ІТ Racing (#{@next_race})")
+    mail(to: user.email, subject: "Наступна гонка ІТ Racing (#{@next_race})")
   end
 
 end
